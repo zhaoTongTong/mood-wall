bb85511fe715337ab1206c16075daa7d8718bee2
zhaotongtong <zhaotongtong@58ganji.com>
2018-08-02 19:35:45 +0800

    feat: 定义数据格式

---
+++ mock-data.js
@@ -0,0 +1,26 @@
+const data = {
+  baseInfo: {
+    nickName: "", // 昵称
+    faceUrl: "",  //头像
+    sign: "", //签名
+    pageView: 2   //浏览量
+  },
+  moodList: [
+    {
+      mood: "",    // 心情
+      moodImages: [],    //图片
+      isPrivate: false,    //是否加密
+      date: "2018-08-01",   //日期
+      comment: []   //评论
+    },
+    {
+      mood: "",
+      moodImages: [],
+      isPrivate: false,
+      date: "2018-08-01",
+      comment: []
+    }
+  ]
+};
+
+export default data
